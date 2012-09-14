38af89f8928c63a7c639cfd694508a7c9635af8f
wesley rocha <wesley.rocha@seatecnologia.com.br>
2012-09-14 17:11:51 -0300

    colocando gráficos correspondentes a cada tema para funciona, agora eles são correspondentes ao json

--- public/javascript/copa2014.js
+++ public/javascript/copa2014.js
@@ -1,33 +1,18 @@
 jQuery(function() {
 	
 	Copa2014.investimentosPorTema({
-		onSuccess: function(json) {
-			console.log(json);
-			Wheel("aeroportos", 100, [json.aeroportos.previsto, json.aeroportos.contratado, json.aeroportos.executado]);
-			Wheel("estadios", 100, [json.estádio.previsto, json.estádio.contratado, json.estádio.executado]);
-			Wheel("hotelaria", 100, [json.hotelaria.previsto, json.hotelaria.contratado, json.hotelaria.executado]);
-			Wheel("mobilidade", 100, [json.mobilidade.previsto, json.mobilidade.contratado, json.mobilidade.executado]);
-			Wheel("portos", 100, [json.portos.previsto, json.portos.contratado, json.portos.executado]);
-			Wheel("seguranca", 100, [json.segurança.previsto, json.segurança.contratado, json.segurança.executado]);
-			Wheel("telecomunicacoes", 100, [json.telecomunicações.previsto, json.telecomunicações.contratado, json.telecomunicações.executado]);
+		onSuccess: function(json) {      
+      geraCirculo("aeroportos", json.aeroportos);
+      geraCirculo("estadios", json.estádio);
+      geraCirculo("hotelaria", json.hotelaria);
+      geraCirculo("mobilidade", json.mobilidade);
+      geraCirculo("portos", json.portos);
+      geraCirculo("seguranca", json.segurança);
+      geraCirculo("telecomunicacoes", json.telecomunicações);
 			
 		},
 		onError: function(xhr, message, error) {
 			console.log("error: " + data);
 		}
 	})
-
-/*	
-	urbana - 188 
-	mobilidade - 188 
-	aeroportos - 65 
-	segurança - 54 
-	pública - 54 
-	estádio - 50 
-	arena - 50 
-	portos - 21 
-	hotelaria - 12
-	telecomunicações - 8
-*/
-
 });
