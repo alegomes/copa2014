5e0e3274cc7da4ddb2f103e27bfe32985af40165
Célio Fonseca <celio.la@gmail.com>
2012-10-08 16:39:38 -0300

    Configurando o Sinatra para utilizar DB

---
+++ db/migrate/20121008184739_create_investimentos.rb
@@ -0,0 +1,16 @@
+class CreateInvestimentos < ActiveRecord::Migration
+  def self.up
+  	puts 'Aew'
+  	create_table :investimentos do |t|
+  		t.string :tema
+  		t.float :valor_previsto
+  		t.float :valor_contratado
+  		t.float :valor_executado
+  		t.datetime :created_at
+  	end
+  end
+
+  def self.down
+  	drop_table :investimentos
+  end
+end
