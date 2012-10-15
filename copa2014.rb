5e9c064001fab33addba96b61968f2f4e5538bcc
Célio Fonseca <celio.la@gmail.com>
2012-10-15 17:23:07 -0300

    Template e refactoring na estrutura.

--- copa2014.rb
+++ copa2014.rb
@@ -28,17 +28,34 @@ configure do
     config.project_path = File.dirname(__FILE__)
     config.sass_dir = 'assets'
   end
-
   set :scss, Compass.sass_engine_options
 end
-
 get '/main.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss :"../assets/stylesheet/main"
 end
+get '/javascript/*' do
+  content_type 'text/javascript', :charset => 'utf-8'
+  params[:splat].each do |js|
+    send_file File.open(File.dirname(__FILE__)+"/assets/javascript/"+js)
+  end
+end
+get '/images/*' do
+  content_type 'image/png', :charset => 'utf-8'
+  params[:splat].each do |image|
+    send_file File.open(File.dirname(__FILE__)+"/assets/images/"+image)
+  end
+end
+
 
 
 get "/" do
   # @invs = Investimentos.where tema: :aeroporto
-	erb :index
+	erb :index, layout: :layout
 end
+
+get "/proporcao-de-valores" do
+  erb :proporcao_valores, layout: :layout
+end
+
+
