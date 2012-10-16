2f99ecd60d02c3cb3b03a1287992741d15429857
Célio Fonseca <celio.la@gmail.com>
2012-10-15 21:47:20 -0300

    Migration finalizada

--- db/migrate/20121015224248_create_investimentos_schema.rb
+++ db/migrate/20121015224248_create_investimentos_schema.rb
@@ -1,13 +1,13 @@
 class CreateInvestimentosSchema < ActiveRecord::Migration
   def up
-  	# create_table :investimentos do |t|
-  	# 	t.string :tema
-  	# 	t.float :valor_previsto
-  	# 	t.float :valor_contratado
-  	# 	t.float :valor_executado
+  	create_table :investimentos do |t|
+  		t.string :tema
+  		t.float :valor_previsto
+  		t.float :valor_contratado
+  		t.float :valor_executado
   		
-  	# 	t.timestamps
-  	# end
+  		t.timestamps
+  	end
   end
 
   def down
