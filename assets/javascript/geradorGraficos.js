1ea5377513e0c8f9d5f782a5ff7ece6d7d4a08e0
Célio Fonseca <celio.fonseca@seatecnologia.com.br>
2012-11-07 16:03:51 -0200

    Apresentacao dos dados de cada cidade como grafico no mapa.

--- assets/javascript/geradorGraficos.js
+++ assets/javascript/geradorGraficos.js
@@ -1,76 +1,80 @@
-function geraCirculo(value, json) {
-    var r = Raphael(value, 204, 204),
-        R = 89,
+function geraCirculo(value, width, height, radius, position_variation, line_weight, line_weight_delta, json) {
+
+    var r = Raphael(value, width, height),
+        R = radius,
         init = true,
         param1 = {
             stroke: "#fff",
-            "stroke-width": 24,
+            "stroke-width": line_weight,
             "box-shadow": "0 0 10px #000"
         },
         param2 = {
             stroke: "#fff",
-            "stroke-width": 20
+            "stroke-width": line_weight - line_weight_delta
         },
         param3 = {
             stroke: "#fff",
-            "stroke-width": 16
+            "stroke-width": line_weight - 2*line_weight_delta
         },
         hash = document.location.hash;
 
     // Custom Attribute
     r.customAttributes.arc = function (value, total, R, colorParam) {
-     	var alpha = 360 / total * value,
-     	  a = (90 - alpha) * Math.PI / 180,
-     	  x = 102 + R * Math.cos(a),
-     	  y = 102 - R * Math.sin(a),
-     	  color = "hsb(".concat(Math.round(R) / 186, ",", value / total, ", .75)"), path;
+        var alpha = 360 / total * value,
+          a = (90 - alpha) * Math.PI / 180,
+          x = position_variation + R * Math.cos(a),
+          y = position_variation - R * Math.sin(a),
+          color = "hsb(".concat(Math.round(R) / 186, ",", value / total, ", .75)"), path;
 
-     	var color = colorParam;
+        var color = colorParam;
 
-     	if (total == value) {
-     		path = [["M", 102, 102 - R], ["A", R, R, 0, 1, 1, 101.99, 102 - R]];
-     	} else {
-     		path = [["M", 102, 102 - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
-     	}
-     	return {path: path, stroke: color};
+        if (total == value) {
+            path = [["M", position_variation, position_variation - R], ["A", R, R, 0, 1, 1, position_variation - 0.01, position_variation - R]];
+        } else {
+            path = [["M", position_variation, position_variation - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
+        }
+        return {path: path, stroke: color};
     };
 
     var red = r.path().attr(param1).attr({arc: [0, 100, R, "rgba(255, 0, 0, 1)"]});
     var yellow = r.path().attr(param2).attr({arc: [0, 100, R, "rgba(0, 0, 255, 1)"]});
     var green = r.path().attr(param3).attr({arc: [0, 100, R, "rgba(0, 255, 0, 1)"]});
 
-	function updateVal(value, total, R, hand, color) {
-		if (total == 31) {
-			// month
-			var d = new Date;
-			d.setDate(1);
-			d.setMonth(d.getMonth() + 1);
-			d.setDate(-1);
-			total = d.getDate();
-		}
+    function updateVal(value, total, R, hand, color) {
+        if (total == 31) {
+            // month
+            var d = new Date;
+            d.setDate(1);
+            d.setMonth(d.getMonth() + 1);
+            d.setDate(-1);
+            total = d.getDate();
+        }
 
-		// var color = "hsb(".concat(Math.round(R) / 100, ",", value / total, ", .75)");
+        // var color = "hsb(".concat(Math.round(R) / 100, ",", value / total, ", .75)");
 
-		if (init) {
-			hand.animate({arc: [value, total, R, color]}, 900, ">");
-		} else {
-			if (!value || value == total) {
-				value = total;
-				hand.animate({arc: [value, total, R, color]}, 750, "bounce", function () {
-					hand.attr({arc: [0, total, R, color]});
-				});
-			} else {
-				hand.animate({arc: [value, total, R, color]}, 750, "elastic");
-			}
-		}
-	}
+        if (init) {
+            hand.animate({arc: [value, total, R, color]}, 900, ">");
+        } else {
+            if (!value || value == total) {
+                value = total;
+                hand.animate({arc: [value, total, R, color]}, 750, "bounce", function () {
+                    hand.attr({arc: [0, total, R, color]});
+                });
+            } else {
+                hand.animate({arc: [value, total, R, color]}, 750, "elastic");
+            }
+        }
+    }
 
-	(function () {
-		updateVal(json.previsto, json.previsto, R, red, "rgba(179, 42, 47, 1)");
-		updateVal(json.contratado, json.previsto, R, yellow, "rgba(241, 190, 42, 1)");
-		updateVal(json.executado, json.previsto, R, green, "rgba(57, 181, 74, 1)");
-		init = false;
-	})();
+    var contratado = json.contratado <= json.previsto ? json.contratado : json.previsto;
+    var executado = json.executado <= json.previsto ? json.executado : json.previsto;
+
+    (function () {
+        updateVal(json.previsto, json.previsto, R, red, "rgba(179, 42, 47, 1)");
+        updateVal(contratado, json.previsto, R, yellow, "rgba(241, 190, 42, 1)");
+        updateVal(executado, json.previsto, R, green, "rgba(57, 181, 74, 1)");
+        init = false;
+    })();
 }
 
 
--- assets/javascript/tema.js
+++ assets/javascript/tema.js
@@ -2,22 +2,23 @@ jQuery(function() {
 	
 	var tema = json_investimento_tema;
 	var id = jQuery(".circulo").attr("id");
-	geraCirculo(id, {
+	geraCirculo(id, 204, 204, 89, 102, 24, 4, {
     previsto: tema["valor_previsto"],
 		contratado: tema["valor_contratado"],
 		executado: tema["valor_executado"]
-  });
+	});
 
-  // for (var index in cidades_sede) {
-  // 	var id = cidades_sede[index].name+"_circulo";
-  // 	var json = cidades_sede[index].json;
+  for (var index in cidades_sede) {
+		var id = cidades_sede[index].name+"_circulo";
+		var json = cidades_sede[index].json;
 
-  // 	console.log(id);
-  // 	geraCirculo(id, {
-	 //    previsto: json["valor_previsto"],
-		// 	contratado: json["valor_contratado"],
-		// 	executado: json["valor_executado"]
-	 //  });
-  // }
+		console.log(cidades_sede[index].name);
+		console.log(json);
+		geraCirculo(id, 100, 100, 40, 51, 12, 2, {
+			previsto: json["valor_previsto"],
+			contratado: json["valor_contratado"],
+			executado: json["valor_executado"]
+		});
+	}
 
 });
