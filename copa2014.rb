3424ab066c8142d1baa7aa81404b1cbf3d28d276
Célio Fonseca <celio.fonseca@seatecnologia.com.br>
2012-11-26 17:09:39 -0200

    Apresentando apenas os 4 ultimos na home...

--- copa2014.rb
+++ copa2014.rb
@@ -22,20 +22,20 @@ require './models/tema'
 
 get '/main.css' do
   content_type 'text/css', :charset => 'utf-8'
-  #cache_control :public, max_age: 43200  # 12 horas.
+  cache_control :public, max_age: 43200  # 12 horas.
   scss :"../assets/stylesheet/main"
 end
 get '/javascript/*' do
   content_type 'text/javascript', :charset => 'utf-8'
   params[:splat].each do |js|
-    #cache_control :public, max_age: 43200  # 12 horas.
+    cache_control :public, max_age: 43200  # 12 horas.
     send_file File.open(File.dirname(__FILE__)+"/assets/javascript/"+js)
   end
 end
 get '/images/*' do
   content_type 'image/png', :charset => 'utf-8'
   params[:splat].each do |image|
-    #cache_control :public, max_age: 43200  # 12 horas.
+    cache_control :public, max_age: 43200  # 12 horas.
     send_file File.open(File.dirname(__FILE__)+"/assets/images/"+image)
   end
 end
@@ -53,9 +53,17 @@ get "/" do
 
   @empreendimentos.each do |emp_tmp|
     ordered_emp = emp_tmp.second.group_by(&:created_at)
+    
+    # Pega apenas os 4 últimos
+    keys = ordered_emp.keys
+    keys = keys.slice keys.size-4, keys.size
+    ordered_emp_filtered = {}
+    ordered_emp.each do |e|
+      ordered_emp_filtered[e.first] = e.second if keys.include? e.first
+    end
 
     @investimentos[emp_tmp.first.to_sym] = [] unless @investimentos[emp_tmp.first]
-    ordered_emp.each do |empreendimento|
+    ordered_emp_filtered.each do |empreendimento|
       
       investimento_tema = Investimento.new
       empreendimento.second.each do |e|
@@ -69,7 +77,7 @@ get "/" do
     end
   end
 
-  #cache_control :public, max_age: 43200  # 12 horas.
+  cache_control :public, max_age: 43200  # 12 horas.
 	erb :index, layout: :layout, :default_encoding => settings.default_encoding
 end
 
@@ -105,7 +113,7 @@ get "/tema/:tema" do
       @investimento_tema.data = emp.created_at
     end
 
-    #cache_control :public, max_age: 43200  # 12 horas.
+    cache_control :public, max_age: 43200  # 12 horas.
     erb :tema, layout: :layout, :default_encoding => settings.default_encoding
   end
 end
@@ -135,11 +143,11 @@ get "/tema/:tema/cidade-sede/:cidade_sede" do
     end
   end
 
-  #cache_control :public, max_age: 43200  # 12 horas.
+  cache_control :public, max_age: 43200  # 12 horas.
   erb :cidade_sede, layout: :layout, :default_encoding => settings.default_encoding
 end
 
 get "/about" do
-  #cache_control :public, max_age: 43200  # 12 horas.
+  cache_control :public, max_age: 43200  # 12 horas.
   erb :about, layout: :layout, :default_encoding => settings.default_encoding
 end
