23e6e5d39fb24c698189cee8b62736661b1ebfe5
Célio Fonseca <celio.fonseca@seatecnologia.com.br>
2012-12-03 16:11:22 -0200

    Apresentando mensagem quando a pagina não contém empreendimentos...

--- assets/javascript/cidade_sede.js
+++ assets/javascript/cidade_sede.js
@@ -3,7 +3,7 @@ jQuery(function() {
 	var cidade_sede = json_investimento_cidade_sede;
 	var id = jQuery(".circulo").attr("id");
 	geraCirculo(id, 204, 204, 89, 0, 102, 24, 4, 4, {
-    previsto: cidade_sede["valor_previsto"],
+    previsto: cidade_sede["valor_previsto"] > 0 ? cidade_sede["valor_previsto"] : 1,
 		contratado: cidade_sede["valor_contratado"],
 		executado: cidade_sede["valor_executado"]
 	});
